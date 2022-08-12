67aab8a290a45023aeae444467a222fb2eb0e2a3
hariqzein23 <110509009+hariqzein23@users.noreply.github.com>
2022-08-11 20:54:33 -0700

    Add files via upload

---
+++ UPRAK_RAFIQ ALHADI ZEIN _XTKJ1.py
@@ -0,0 +1,8 @@
+nama =input("Masukan Nama Anda: ")
+nama_depan =input("Masukan Nama Depan Anda: ")
+
+print= input("Nama Anda adalah"+ nama_depan + nama)
+
+suhu = input("Masukan Suhu Celcius")
+reamur = ( 4/5 * suhu  )
+print (suhu+ "suhu Celcius Adalah"+ reamur + "reamur")
